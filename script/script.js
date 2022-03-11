72f907e0d255a35ec3ae2854dcb0c2ede8d3b301
rafaelgomesxavier <contato.rafaelgomes@outlook.com>
2022-03-11 02:36:06 -0300

    add scroll to section

--- script/script.js
+++ script/script.js
@@ -43,3 +43,22 @@ function initAccordionList() {
 }
 
 initAccordionList()
+
+function initScrollIntoView(){;
+
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block:  "start",
+    })
+  }
+  internalLinks.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  });
+}
+initScrollIntoView();
